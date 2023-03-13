4b66d79b27bbe94794135d0059b7d9dfc08edcd2
blackcrystal0000 <muthonigachanja2000@gmail.com>
2023-03-13 12:54:32 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+let dogName = "Blurf";
+
+let myCatsName = () => {
+    
+    let firstName = prompt("Enter first name");
+    let secondName = prompt("Enter second name");
+    alert(`my cat is called ${firstName} ${secondName}.`);
+    alert(`the ${dogName} ate the ${firtName}`); 
+}
+
+myCatsName();
