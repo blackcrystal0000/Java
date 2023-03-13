c5ab17f2132da19da03ff901810846a5c490a9ef
blackcrystal0000 <muthonigachanja2000@gmail.com>
2023-03-13 13:05:46 +0300

    second commit

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
-let dogName = "Blurf";
+function calc() {
+    var a = parseInt(document.querySelector("#value1").value);
+    var b = parseInt(document.querySelector("#value2").value);
+    var op = document.querySelector("#operator").value; 
+    var calculate;
 
-let myCatsName = () => {
-    
-    let firstName = prompt("Enter first name");
-    let secondName = prompt("Enter second name");
-    alert(`my cat is called ${firstName} ${secondName}.`);
-    alert(`the ${dogName} ate the ${firtName}`); 
+    if (op == "add") {
+      calculate = a + b;
+    } else if (op == "min") {
+      calculate = a - b;
+    } else if (op == "div") {
+        calculate = a / b;
+    } else if (op == "mul") {
+        calculate = a * b;
+    } else if (op == "rem") {
+        calculate = a % b; 
+    }
+
+    document.querySelector("#result").innerHTML = calculate;
 }
-
-myCatsName();
